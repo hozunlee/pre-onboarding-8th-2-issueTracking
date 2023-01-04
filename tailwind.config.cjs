a2fbbb0d130e2ca02d341de9e11d6d2e3749b672
hozunlee <ho2yahh@gmail.com>
2023-01-04 12:45:51 +0900

    chore : tailwind css 추가

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        main: '#8AAAE5',
+        sub: '#FEFEFE',
+      },
+    },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      // '2xl': '1536px',
+    },
+  },
+  plugins: [],
+};
